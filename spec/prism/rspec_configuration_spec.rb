71b8cf183fcbcf33923d8f6895005191f20cbf21
lionelbarrow <lionelbarrow@gmail.com>
2013-08-11 22:08:35 -0500

    Add another case to configuration test

--- spec/prism/rspec_configuration_spec.rb
+++ spec/prism/rspec_configuration_spec.rb
@@ -3,5 +3,6 @@ require "spec_helper"
 describe RSpec::Core::Configuration do
   it "adds 'prism_enabled' to RSpec::Core::Configuration" do
     RSpec::Core::Configuration.new.should respond_to(:prism_enabled)
+    RSpec::Core::Configuration.new.should respond_to(:prism_enabled=)
   end
 end
