5fb42c861f3dc6e17ba32d11196522c6fa53070e
lionelbarrow <lionelbarrow@gmail.com>
2013-12-28 17:34:51 -0600

    Add some dependencies that were not required

--- lib/prism.rb
+++ lib/prism.rb
@@ -1,3 +1,7 @@
+require "set"
+
+require "prism/example_group_list"
+require "prism/example_group_wrapper"
 require "prism/runner"
 require "prism/stack_frame"
 require "prism/stack_trace_generator"
